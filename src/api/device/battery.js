a220953da67f4f5b68f740ec23ae080f527d4ab5
arsize <arsizeof3416@gmail.com>
2023-10-29 20:22:06 +0800

    feat: 导入电池数据

--- src/api/device/battery.js
+++ src/api/device/battery.js
@@ -1,53 +1,60 @@
-import request from '@/utils/request'
+import request from "@/utils/request";
 
 // 查询电池列表
 export function listBattery(query) {
   return request({
-    url: '/device/battery/list',
-    method: 'get',
-    params: query
-  })
+    url: "/device/battery/list",
+    method: "get",
+    params: query,
+  });
 }
 
 // 查询电池详细
 export function getBattery(id) {
   return request({
-    url: '/device/battery/' + id,
-    method: 'get'
-  })
+    url: "/device/battery/" + id,
+    method: "get",
+  });
 }
 
 // 新增电池
 export function addBattery(data) {
   return request({
-    url: '/device/battery',
-    method: 'post',
-    data: data
-  })
+    url: "/device/battery",
+    method: "post",
+    data: data,
+  });
 }
 
 // 修改电池
 export function updateBattery(data) {
   return request({
-    url: '/device/battery',
-    method: 'put',
-    data: data
-  })
+    url: "/device/battery",
+    method: "put",
+    data: data,
+  });
 }
 
 // 删除电池
 export function delBattery(id) {
   return request({
-    url: '/device/battery/' + id,
-    method: 'delete'
-  })
+    url: "/device/battery/" + id,
+    method: "delete",
+  });
 }
 
 // 导出电池
 export function exportBattery(query) {
   return request({
-    url: '/device/battery/export',
-    method: 'get',
-    params: query
-  })
+    url: "/device/battery/export",
+    method: "get",
+    params: query,
+  });
+}
+// 下载电池导入模板
+export function importTemplate() {
+  return request({
+    url: "/device/battery/importTemplate",
+    method: "get",
+  });
 }
--- src/views/device/battery/index.vue
+++ src/views/device/battery/index.vue
@@ -159,7 +159,6 @@
           plain
           icon="el-icon-download"
           size="mini"
-          :loading="exportLoading"
           @click="handleExport"
           v-hasPermi="['device:battery:export']"
           >导出</el-button
@@ -169,10 +168,9 @@
         <el-button
           type="primary"
           plain
-          icon="el-icon-download"
+          icon="el-icon-upload"
           size="mini"
-          :loading="exportLoading"
-          @click="handleExport"
+          @click="handleImport"
           >导入电池</el-button
         >
       </el-col>
@@ -314,6 +312,49 @@
         <el-button @click="cancel">取 消</el-button>
       </div>
     </el-dialog>
+
+    <!-- 机柜导入 -->
+    <!-- 用户导入对话框 -->
+    <el-dialog
+      :title="upload.title"
+      :visible.sync="upload.open"
+      width="400px"
+      append-to-body
+    >
+      <el-upload
+        ref="upload"
+        :limit="1"
+        accept=".xlsx, .xls"
+        :headers="upload.headers"
+        :action="upload.url + '?updateSupport=' + upload.updateSupport"
+        :disabled="upload.isUploading"
+        :on-progress="handleFileUploadProgress"
+        :on-success="handleFileSuccess"
+        :auto-upload="false"
+        drag
+      >
+        <i class="el-icon-upload"></i>
+        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+        <div class="el-upload__tip text-center" slot="tip">
+          <div class="el-upload__tip" slot="tip">
+            <el-checkbox v-model="upload.updateSupport" />
+            是否更新已经存在的机柜数据
+          </div>
+          <span>仅允许导入xls、xlsx格式文件。</span>
+          <el-link
+            type="primary"
+            :underline="false"
+            style="font-size: 12px; vertical-align: baseline"
+            @click="importTemplate"
+            >下载模板</el-link
+          >
+        </div>
+      </el-upload>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="submitFileForm">确 定</el-button>
+        <el-button @click="upload.open = false">取 消</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -325,7 +366,9 @@ import {
   addBattery,
   updateBattery,
   exportBattery,
+  importTemplate,
 } from "@/api/device/battery";
+import { getToken } from "@/utils/auth";
 
 export default {
   name: "Battery",
@@ -368,6 +411,21 @@ export default {
         remarks: null,
         createTime: null,
         updateTime: null,
+      },
+      // 机柜导入参数
+      upload: {
+        // 是否显示弹出层（机柜导入）
+        open: false,
+        // 弹出层标题（机柜导入）
+        title: "机柜导入",
+        // 是否禁用上传
+        isUploading: false,
+        // 是否更新已经存在的用户数据
+        updateSupport: 0,
+        // 设置上传的请求头部
+        headers: { Authorization: "Bearer " + getToken() },
+        // 上传的地址
+        url: process.env.VUE_APP_BASE_API + "/device/battery/importData",
       },
       // 表单参数
       form: {},
@@ -525,6 +583,32 @@ export default {
           this.exportLoading = false;
         })
         .catch(() => {});
+    },
+    // 导入按钮操作
+    handleImport() {
+      this.upload.open = true;
+    },
+    /** 下载模板操作 */
+    importTemplate() {
+      importTemplate().then((response) => {
+        this.$download.name(response.msg);
+      });
+    },
+    // 文件上传中处理
+    handleFileUploadProgress(event, file, fileList) {
+      this.upload.isUploading = true;
+    },
+    // 文件上传成功处理
+    handleFileSuccess(response, file, fileList) {
+      this.upload.open = false;
+      this.upload.isUploading = false;
+      this.$refs.upload.clearFiles();
+      this.$alert(response.msg, "导入结果", { dangerouslyUseHTMLString: true });
+      this.getList();
+    },
+    // 提交上传文件
+    submitFileForm() {
+      this.$refs.upload.submit();
     },
   },
 };
